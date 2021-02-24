1c5af5b5b7b0f17e7d6243717fa05cdcdc4858fd
Shaundel5597 <naunie08@yahoo.com>
2021-02-23 23:07:24 -0500

    variables hw

---
+++ homework.js
@@ -0,0 +1,59 @@
+let myVariable = "shaundel";
+
+console.log(myVariable);
+
+let myVariableIsThisNumber = 55
+
+console.log(myVariableIsThisNumber)
+
+true
+
+console.log(true)
+
+let carName = "Volvo"
+
+console.log(carName)
+
+let num = 50
+
+console.log(num)
+
+let num1 = 5
+
+console.log(num1)
+
+ let num2 = 10 
+
+ console.log(num2)
+
+ let num3 = 15
+
+ console.log(num3)
+
+ let animalAge = 17
+
+ console.log(animalAge)
+
+ let animalName = "rufus"
+
+ console.log(animalName)
+
+let animalHeight = 18
+
+console.log(animalHeight)
+
+let animalSkin = "spotted"
+
+console.log(animalSkin)
+
+let animalColor = "tan"
+
+console.log(animalColor)
+
+let animalWeight = 1500
+
+console.log(animalWeight)
+
+let AnimalPattern = "striped"
+
+console.log(AnimalPattern)
