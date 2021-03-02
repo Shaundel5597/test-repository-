0de184e1377bf4e3009be0cc603bc37408ca2ab5
Shaundel5597 <naunie08@yahoo.com>
2021-03-02 00:12:42 -0500

    1-mar-hw.js

---
+++ Classwork/mar1.js
@@ -0,0 +1,21 @@
+/*
+    Array methods practice (pairs).
+        - Create an array that contains two of your favorite things to do.
+        - Add another thing to that list using an array method.
+        - Sort the array.
+        - Reverse the order of the array.
+*/
+
+let firstArr = ["workout", "Hunting"]
+
+console.log(firstArr)
+
+firstArr.push("Drawing")
+
+console.log(firstArr, "<--- Adding")
+
+firstArr.sort()
+
+console.log(firstArr, "<--- Sorting")
+
+console.log(firstArr.reverse(), "<--- Reversing")
