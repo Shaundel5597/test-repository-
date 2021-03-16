90a7a128d423e8d2a72d3ca5aa8834c286fc990e
Shaundel5597 <naunie08@yahoo.com>
2021-03-16 12:12:51 -0400

    3.16.21codewars.js

--- CodeWars/3.16.21codewars.js
+++ CodeWars/3.16.21codewars.js
@@ -10,7 +10,22 @@ function nthSmallest(arr, pos){
          let oddNum = num % 2 == 0
           if (evenNum > oddNum)
          return "Even the greater than Odd" 
-        } else { return "Odd is greater than Even"
+        else return "Odd is greater than Even"
     }
-          
         
+   
+   
+    function hydrate(s) {
+        // your code here
+        let sum = 0
+        let newList = s.split(" ")
+        
+          for (let i = 0; i < newList.length; i++){
+            if (Number(newList[i])){
+          }
+        }
+        if (sum > 1){
+          return sum + " glasses of water"
+        }
+        return sum + " glass of water"
+      }
